7acbfe03b344a4122ce81d2465b49e8fc44f7e80
Timothy Adebisi <adebisis539@gmail.com>
2024-09-12 19:39:07 +0100

    create database connection and depedency

--- database.py
+++ database.py
@@ -1,4 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import settings
 
+DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
